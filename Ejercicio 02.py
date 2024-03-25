8afcb348a4bf270356c7aa226578413c86c1bfb0
RamiroSilvaM <66190838+Ramasilva16@users.noreply.github.com>
2024-03-25 16:33:50 -0300

    Update Ejercicio 02.py

--- Ejercicio 02.py
+++ Ejercicio 02.py
@@ -1,6 +1,6 @@
 edad = int (input ('Ingrese la edad:'))
 distancia = int (input ('Ingrese la distancia a la cual se encuentra, en KM:'))
-if (edad<=70 and edad>18 and distancia <=500):
+if (edad<=70 and edad>=18 and distancia <=500):
     print('Vota')
 else:
     print('No vota')
