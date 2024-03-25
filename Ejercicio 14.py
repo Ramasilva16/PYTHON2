dadb0b2106a7e2e5cca3fdb645e4a56ee71dc913
RamiroSilvaM <66190838+Ramasilva16@users.noreply.github.com>
2024-03-25 16:31:46 -0300

    Add files via upload

---
+++ Ejercicio 14.py
@@ -0,0 +1,10 @@
+n1 = int ( input("Ingrese un número"))
+n2 = int ( input("Ingrese un número"))
+
+a=0
+b=0
+
+if (n1 < n2):
+    print (n1, "Es menor que ",n2)
+else:
+    print (n1, "Es mayor que ",n2)
